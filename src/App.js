cf4f905271064890fb9d17cdaa0703888177bde0
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-13 22:53:19 +0330

    adding the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import { Data } from './data';
 
 function App() {
+
+  const [products, setProducts]= useState(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {
+          products.map(item=> {
+            return(
+              <img key={item.id} src={item.image}/>
+            )
+          })
+        }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
